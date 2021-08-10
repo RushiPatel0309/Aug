09efac1228bb9279ad595ffadb0b1ad65b50aac0
RushiPatel0309 <Brinshi2011>
2021-08-10 23:19:05 +0100

    Jira - TestAutomation-1234 Adding two new Classes for  Demo

---
+++ src/uk/testing/sotware/gitdemo/AppClass1.java
@@ -0,0 +1,7 @@
+package uk.testing.sotware.gitdemo;
+
+public class AppClass1 {
+        public void doSomething(){
+            System.out.println("Doing Something");
+        }
+    }
---
+++ src/uk/testing/sotware/gitdemo/AppClass2.java
@@ -0,0 +1,7 @@
+package uk.testing.sotware.gitdemo;
+
+public class AppClass2 {
+    public void doSomething(){
+        System.out.println("Doing something from App Class2");
+    }
+}

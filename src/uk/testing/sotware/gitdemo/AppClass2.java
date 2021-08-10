958c39f930511665524734c05c4832271a4d7dc1
RushiPatel0309 <Brinshi2011>
2021-08-10 23:24:16 +0100

    Jira -234 Adding new Test method

--- src/uk/testing/sotware/gitdemo/AppClass2.java
+++ src/uk/testing/sotware/gitdemo/AppClass2.java
@@ -4,4 +4,7 @@ public class AppClass2 {
     public void doSomething(){
         System.out.println("Doing something from App Class2");
     }
+    public void testUpdate(){
+        System.out.println("Adding another test Method");
+    }
 }
